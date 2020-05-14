e2dfb528dfa252fadbf85cc862aed995379ee9a6
Tim Hemel <tim@securesoftware.nl>
2020-05-14 20:29:32 +0200

    Extra prolog callbacks

--- XMLAnalyzer.py
+++ XMLAnalyzer.py
@@ -14,6 +14,9 @@ class XMLAnalyzer:
 
     def set_prolog_base_functions(self):
         self.query_engine.register_function('xpath', self.get_xpath_value)
+        self.query_engine.register_function('attr', self.attr)
+        self.query_engine.register_function('lowercase', self.lowercase)
+        self.query_engine.register_function('version_at_least', self.version_at_least)
 
     def add_rules(self, compiled_rules):
         self.query_engine.load_script_from_string(compiled_rules, overwrite=False)
@@ -32,13 +35,37 @@ class XMLAnalyzer:
         
     def get_xpath_value(self, query, variable):
         try:
+            self._debug("DEBUG: query: %s" % (to_python(query)))
             r = self.xml_tree.xpath(to_python(query))
             for y in r:
-                self._debug("element", repr(y))
+                self._debug("DEBUG: %s = %s" % (to_python(query), repr(y)))
                 for _ in unify(variable, self.query_engine.atom(y)):
                     yield False
         except lxml.etree.XPathEvalError as e:
-            print("ERROR:", e)
+            print("ERROR (xpath):", e, to_python(query))
+
+    def attr(self, element, key, value):
+        elt = to_python(element)
+        self._debug("element", repr(elt))
+        for k,v in elt.items():
+            for x in unify(key, self.query_engine.atom(k)):
+                for y in unify(value, self.query_engine.atom(v)):
+                        self._debug("DEBUG: attr: %s=%s" % (k,v))
+                        yield False
+
+    def lowercase(self, val1, val2):
+        self._debug("DEBUG: lowercase: %s=%s" % (val1,val2))
+        # val1 must be instantiated
+        v = self.query_engine.atom(to_python(val1).lower())
+        for x in unify(val2, v):
+            yield False
+
+    def version_at_least(self, version1, version2):
+        # version1 and version2 must be instantiated
+        v1 = to_python(version1).split('.')
+        v2 = to_python(version2).split('.')
+        if v2 >= v1:
+            yield False
 
     def _debug(self, *msg):
         if self.debug:
