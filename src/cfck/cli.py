dea8c9c833005efe97595b51f8fe1c4a2dd097d4
Tim Hemel <tim@securesoftware.nl>
2023-01-18 13:00:41 +0100

    use defusedxml to validate/sanitize XML input

--- src/cfck/cli.py
+++ src/cfck/cli.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env python3
 
-#
-# fsce: find security configuration errors
-#
-#
-
-#from lxml import etree
-from defusedxml import ElementTree as etree
+from lxml import etree
+from defusedxml import ElementTree as defused_etree
+from defusedxml.common import DefusedXmlException
 import click
+import sys
+import io
 from yldprolog.compiler import compile_prolog_from_file
 
-from XMLAnalyzer import XMLAnalyzer
+from .XMLAnalyzer import XMLAnalyzer
 
-def render_finding(f, finding_vars):
+def render_finding(filename, finding_vars):
     line = finding_vars[1].sourceline
-    filename = f.name
     message = finding_vars[0].format(*finding_vars[1:])
     return f'{filename}:{line}:0:{message}'
+
+def secure_parse(inp_fn):
+    '''this function will parse the XML with the defusedxml library to validate/sanitize the input for lxml'''
+    t = defused_etree.parse(inp_fn)
+    out_f = io.StringIO()
+    t.write(out_f, encoding='unicode')
+    s = out_f.getvalue()
+    t = etree.fromstring(s)
+    return t
+
 
 @click.command()
 @click.option('-r','--rules', type=click.Path())
 @click.option('-d','--debug', type=bool, is_flag=True, default=False)
-@click.argument('inp', nargs=-1, type=click.File('r'))
+@click.argument('inp', nargs=-1, type=click.Path())
 def analyze(rules, debug, inp):
-    # print(f'{t=}')
-    # print(f'tree={t.getroot()}')
-
     compiled_rules = compile_prolog_from_file(rules)
 
     x = XMLAnalyzer()
-    for inp_file in inp:
+    x.add_rules(compiled_rules)
+    for inp_fn in inp:
         # print(f'Processing {inp_file.name}')
-        t = etree.parse(inp_file)
-        x.set_xml(t)
-        x.set_debug(debug)
+        try:
+            t = secure_parse(inp_fn)
+            x.set_xml(t)
+            x.set_debug(debug)
 
-        x.add_rules(compiled_rules)
+            r = x.ask('q')
 
-        r = x.ask('q')
-
-        for y in r:
-            print(render_finding(inp_file, y))
-            # print(dir(y[1]))
-            # print(y[1].sourceline)
-            # print(y[0].format(*y[1:]))
-
+            for y in r:
+                print(render_finding(inp_fn, y))
+        except DefusedXmlException as e:
+            raise click.ClickException(f'Insecure XML: {e}')
 
 if __name__=="__main__":
     analyze()
