06f254a18ebd5766e673c8c78b56c107b7d62f2c
Tim Hemel <tim@securesoftware.nl>
2023-03-01 17:15:49 +0100

    fix SARIF output, add rules to SARIF file, rename sarif_om fields to SARIF fields

--- src/cfck/SarifRenderer.py
+++ src/cfck/SarifRenderer.py
@@ -1,12 +1,40 @@
 import logging
 
 import json
+import bisect
 from attrs import asdict
-from sarif_om import SarifLog, Run, Tool, ToolComponent, Result, PhysicalLocation, ArtifactLocation, Region
+from attrs import has, fields
+import cattrs
+from cattrs.gen import make_dict_unstructure_fn, override
+from sarif_om import SarifLog, Run, Tool, ToolComponent, Result, PhysicalLocation, ArtifactLocation, Region, ReportingDescriptor, Message
 
 from .BaseRenderer import BaseRenderer
 
 logger = logging.getLogger(__name__)
+
+def rename_schema_field(a):
+    try:
+        return a.metadata['schema_property_name']
+    except KeyError:
+        return a.name
+
+def to_schema_fields_unstructure(cls):
+    return make_dict_unstructure_fn(cls, converter, _cattrs_omit_if_default=True, **{
+        a.name: override(rename=rename_schema_field(a)) for a in fields(cls)
+    })
+
+converter = cattrs.Converter()
+converter.register_unstructure_hook_factory(has, to_schema_fields_unstructure)
+
+def clear_none_values(obj):
+    if isinstance(obj,dict):
+        return dict((k,clear_none_values(v)) for k,v in obj.items() if v is not None)
+    elif isinstance(obj,list):
+        return list(clear_none_values(v) for v in obj)
+    elif isinstance(obj,tuple):
+        return tuple(clear_none_values(v) for v in obj)
+    else:
+        return obj
 
 class SarifRenderer(BaseRenderer):
 
@@ -21,14 +49,26 @@ class SarifRenderer(BaseRenderer):
     def feed(self, filename, iterator):
         self.sarif_log.runs[0].results += [ self.render_func(filename, finding_vars) for finding_vars in iterator ]
 
+    def add_rules(self):
+        for run in self.sarif_log.runs:
+            rule_ids = sorted([result.rule_id for result in run.results ])
+            run.tool.driver.rules = [ ReportingDescriptor(id=rid, name=rid) for rid in rule_ids ]
+            for result in run.results:
+                rule_index = bisect.bisect_left(rule_ids, result.rule_id)
+                result.rule_index = rule_index
+
     def end(self):
-        #print(self.sarif_log)
-        print(json.dumps(asdict(self.sarif_log, filter=(lambda a,v: (v is not None)))))
+        self.add_rules()
+        renamed_log = clear_none_values(converter.unstructure(self.sarif_log))
+        print(json.dumps(renamed_log))
+
 
 def sarif_location(path, startloc, endloc):
+    logger.debug(f'sarif_location: {path=}, {startloc=}, {endloc=}')
     region = Region(start_line=startloc[0], start_column=startloc[1], end_line=endloc[0], end_column=endloc[1], byte_offset=None, char_offset=None)
     artifact_location = ArtifactLocation(uri=path, index=None)
-    loc = PhysicalLocation(artifact_location=artifact_location, region=region)
+    loc = { 'physicalLocation': PhysicalLocation(artifact_location=artifact_location, region=region) }
+    logger.debug(f'sarif_location: {loc=}')
     return loc
 
 def sarif_finding(filename, query_vars):
@@ -39,8 +79,9 @@ def sarif_finding(filename, query_vars):
     level = query_vars[1]
     message = query_vars[2].format(*query_vars[4:])
     locations = [ sarif_location(*loc) for loc in query_vars[3] ]
+    logger.debug(f'{locations=}')
     # TODO: kind
-    result = Result(rule_id=rule_id, level=level, message=message, locations=locations, kind=None)
+    result = Result(rule_id=rule_id, level=level, message=Message(text=message), locations=locations, kind=None)
     return result
 
 
